4f0a5e59614259f93b40a1bfdf78279c37174973
shixuanhong <310303892@qq.com>
2022-01-28 23:44:58 +0800

    refactor: directory

--- example/main.ts
+++ example/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import variables from './assets/style/variables.scss?export'
-console.log(variables)
+
+import variables2 from './assets/style/test.module.scss'
+
+console.log(variables,variables2)
 createApp(App).mount('#app')
